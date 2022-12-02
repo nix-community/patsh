6b7c94dc68eba6afb08461e03254ca2123843489
figsoda <figsoda@pm.me>
2022-12-01 22:46:24 -0500

    flake: add checks

--- flake.nix
+++ flake.nix
@@ -4,38 +4,48 @@
   inputs = {
     crane = {
       url = "github:ipetkov/crane";
+      inputs.flake-utils.follows = "flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-compat.follows = "crane";
       inputs.rust-overlay.follows = "crane";
     };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-analyzer-src.follows = "fenix";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, crane, nixpkgs }:
-    let
-      inherit (nixpkgs.lib) genAttrs optional systems;
-    in
-    {
-      formatter = genAttrs systems.flakeExposed
-        (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
+  outputs = { self, crane, fenix, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (crane.lib.${system}.overrideToolchain fenix.packages.${system}.default.toolchain)
+          buildDepsOnly buildPackage cargoClippy cargoFmt cleanCargoSource;
+        inherit (nixpkgs.legacyPackages.${system}) libiconv nixpkgs-fmt stdenv;
+        inherit (nixpkgs.lib) optional;
 
-      packages = genAttrs
-        [
-          "aarch64-darwin"
-          "aarch64-linux"
-          "x86_64-darwin"
-          "x86_64-linux"
-        ]
-        (system:
-          let
-            inherit (nixpkgs.legacyPackages.${system}) libiconv stdenv;
-            inherit (crane.lib.${system}) buildPackage cleanCargoSource;
-          in
-          {
-            default = buildPackage {
-              src = cleanCargoSource self;
-              buildInputs = optional stdenv.isDarwin libiconv;
-            };
+        args' = {
+          src = cleanCargoSource self;
+          buildInputs = optional stdenv.isDarwin libiconv;
+        };
+
+        args = args' // {
+          cargoArtifacts = buildDepsOnly args';
+        };
+      in
+      {
+        checks = {
+          build = self.packages.${system}.default;
+          clippy = cargoClippy (args // {
+            cargoClippyExtraArgs = "-- -D warnings";
           });
-    };
+          fmt = cargoFmt args;
+        };
+
+        formatter = nixpkgs-fmt;
+
+        packages.default = buildPackage args;
+      });
 }
