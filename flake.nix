88e2bdf50a133558d62ff9aeee727c36f8e6a3ac
figsoda <figsoda@pm.me>
2022-12-01 21:59:54 -0500

    add missing build input on darwin

--- flake.nix
+++ flake.nix
@@ -11,22 +11,28 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, crane, nixpkgs }: {
-    packages = nixpkgs.lib.genAttrs
-      [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ]
-      (system:
-        let
-          inherit (crane.lib.${system}) buildPackage cleanCargoSource;
-        in
-        {
-          default = buildPackage {
-            src = cleanCargoSource self;
-          };
-        });
-  };
+  outputs = { self, crane, nixpkgs }:
+    let
+      inherit (nixpkgs.lib) genAttrs optional;
+    in
+    {
+      packages = genAttrs
+        [
+          "aarch64-darwin"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "x86_64-linux"
+        ]
+        (system:
+          let
+            inherit (nixpkgs.legacyPackages.${system}) libiconv stdenv;
+            inherit (crane.lib.${system}) buildPackage cleanCargoSource;
+          in
+          {
+            default = buildPackage {
+              src = cleanCargoSource self;
+              buildInputs = optional stdenv.isDarwin libiconv;
+            };
+          });
+    };
 }
