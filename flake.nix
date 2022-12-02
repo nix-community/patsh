0289ff7f590ef09d6f7bc2d7cfb98a244144b686
figsoda <figsoda@pm.me>
2022-12-01 21:51:52 -0500

    fix ci

--- flake.nix
+++ flake.nix
@@ -2,32 +2,31 @@
   description = "A command-line tool for patching shell scripts";
 
   inputs = {
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-compat.follows = "crane";
+      inputs.rust-overlay.follows = "crane";
+    };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      inherit (nixpkgs.lib) genAttrs importTOML;
-    in
-    {
-      packages = genAttrs
-        [
-          "aarch64-darwin"
-          "aarch64-linux"
-          "x86_64-darwin"
-          "x86_64-linux"
-        ]
-        (system:
-          let
-            inherit (nixpkgs.legacyPackages.${system}) rustPlatform;
-          in
-          {
-            default = rustPlatform.buildRustPackage {
-              pname = "patsh";
-              inherit ((importTOML (self + "/Cargo.toml")).package) version;
-              src = self;
-              cargoLock.lockFile = self + "/Cargo.lock";
-            };
-          });
-    };
+  outputs = { self, crane, nixpkgs }: {
+    packages = nixpkgs.lib.genAttrs
+      [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ]
+      (system:
+        let
+          inherit (crane.lib.${system}) buildPackage cleanCargoSource;
+        in
+        {
+          default = buildPackage {
+            src = cleanCargoSource self;
+          };
+        });
+  };
 }
